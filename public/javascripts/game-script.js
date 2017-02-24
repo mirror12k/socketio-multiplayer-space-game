673b68284618ce5450140c43c85dc3acfeacbd26
mirror <mirror12k@gmail.com>
2017-02-24 17:52:55 -0500

    - work on implementing basic game drawing and keystate tracking

--- public/javascripts/game-script.js
+++ public/javascripts/game-script.js
@@ -5,9 +5,61 @@ var socket;
 var clientState = {};
 var gameState;
 
+var keyState = {
+	W: false,
+	A: false,
+	S: false,
+	D: false,
+};
+
+function drawGame() {
+	var canvas = $('#gameCanvas')[0];
+	var ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = '#FFFFFF';
+	ctx.fillRect(0, 0, 640, 400);
+
+	ctx.fillStyle = '#FF00FF';
+	ctx.fillRect(gameState.me.x, gameState.me.y, 20, 20);
+
+	for (var asteroid in gameState.asteroids) {
+		asteroid = gameState.asteroids[asteroid];
+		ctx.fillStyle = '#FFAA00';
+		ctx.fillRect(asteroid.x, asteroid.y, 20, 20);
+
+	}
+}
+
+function updateGame() {
+	drawGame();
+
+	// console.log("debug keystate: ", keyState);
+	if (keyState.A) {
+		gameState.me.x -= 4;
+	} else if (keyState.D) {
+		gameState.me.x += 4;
+	}
+	if (keyState.W) {
+		gameState.me.y -= 4;
+	} else if (keyState.S) {
+		gameState.me.y += 4;
+	}
+}
+
 function startClientGame(state) {
 	gameState = state;
 	socket.emit('state_update', clientState);
+
+	setInterval(updateGame, 30);
+
+	$('#gameCanvas').keydown(function (e) {
+		var key = String.fromCharCode(e.keyCode);
+		keyState[key] = true;
+	});
+	$('#gameCanvas').keyup(function (e) {
+		var key = String.fromCharCode(e.keyCode);
+		keyState[key] = false;
+	});
 }
 
 function startClientConnection(username) {
--- routes/client-connections.js
+++ routes/client-connections.js
@@ -8,7 +8,14 @@ function startServer(httpServer) {
 	var server = socketio.listen(httpServer);
 	server.sockets.on('connection', onSocketIOConnection);
 
-	var gameState = {};
+	var gameState = {
+		players: {},
+		asteroids: {
+			1: { x: 100, y: 100 },
+			2: { x: 200, y: 150 },
+			3: { x: 100, y: 300 },
+		},
+	};
 
 	function onSocketIOConnection(socket) {
 		console.log('got client connection');
@@ -19,7 +26,9 @@ function startServer(httpServer) {
 			console.log('got authentication from client as ' + data.username);
 			socket.gameClient.username = data.username;
 			socket.gameClient.authenticated = true;
-			socket.emit('authenticated');
+
+			gameState.players[socket.id] = { x: 5.0, y: 5.0 };
+			socket.emit('authenticated', { me: gameState.players[socket.id], asteroids: gameState.asteroids });
 		});
 		socket.on('state_update', function (client_state) {
 			if (socket.gameClient.authenticated) {
